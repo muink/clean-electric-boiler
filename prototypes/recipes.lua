3919659989515303bf31f03cb2ca5858ee4e67d7
muink <hukk1996@gmail.com>
2019-03-08 10:30:22 +0800

    调整合成时间

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -5,7 +5,7 @@ data:extend({
     name = "clean-electric-boiler-165",
     enabled = "false",
     ingredients = {{"steel-plate", 10}, {"pipe", 10}, {"copper-plate", 20}, {"advanced-circuit", 2}},
-    energy_required = 1,
+    energy_required = 5,
     result = "clean-electric-boiler-165"
   },
   {
@@ -13,7 +13,7 @@ data:extend({
     name = "clean-electric-boiler-500",
     enabled = "false",
     ingredients = {{"steel-plate", 10}, {"pipe", 10}, {"copper-plate", 20}, {"advanced-circuit", 5}},
-    energy_required = 1,
+    energy_required = 5,
     result = "clean-electric-boiler-500"
   }
 })
